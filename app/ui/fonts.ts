47bf9a76d6aaf887f7d36888d5dccb3771342d8a
Catailog <tlsdudghks11@gmail.com>
2026-05-09 00:10:02 +0900

    feat: 글꼴 추가

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana, DotGothic16 } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+export const dotGothic16 = DotGothic16({ weight: ["400"], subsets: ["latin"] });
